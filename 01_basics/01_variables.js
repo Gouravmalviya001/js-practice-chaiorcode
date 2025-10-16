c49a1b64c150ba94d203f4de99e7d3afcbc55598
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-16 09:43:50 +0000

    Learn About Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+let accountId = 1221988293;
+const accountName = "gourav@123";
+var accountPassword = 9893734080;
+
+accountCity = "Bhopal"; //bad habit
+
+let accountState; // Value = Undefined
+
+// accountName = "Einstein123";
+
+// console.log(accountName); // Constant Variable will never change...
+
+/*
+ Prefer not to use var 
+ because of issue in faunctional block scope
+*/
+
+console.table([accountId,accountName,accountPassword,accountCity,accountState]);
