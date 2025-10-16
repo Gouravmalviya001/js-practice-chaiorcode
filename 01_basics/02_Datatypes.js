887e63f4fa79f09296d89b85527ee7cfc93be2f7
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-16 10:12:00 +0000

    Learn about Datatypes in js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as a newer version
+
+// alert(3 + 3); // we are using Node.js not browser
+
+// console.log(3 +
+//     3);
+// )              //Not good way // code is not understandable
+
+//**************DATATYPES**************** */
+
+let name = "Gourav"; //string
+let age = 24; //number
+let isLoggedIn = false;//boolean
+let salary = 25000.00; //bigInt
+let comapanyName; //undefined
+// symbol => unique //Generally use in React
+//null = standalone value
+//object
+
+console.log(typeof(undefined));
+
+console.log(typeof(null));
+
