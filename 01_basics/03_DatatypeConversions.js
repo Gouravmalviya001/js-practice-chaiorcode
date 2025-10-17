35f30cee80051f9e0c936d4cf6fb126e4f5dec1f
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-17 09:20:43 +0000

    conversions complete.

--- 01_basics/03_DatatypeConversions.js
+++ 01_basics/03_DatatypeConversions.js
@@ -26,3 +26,38 @@ let number = 33;
 let numberInString = String(number);
 console.log(numberInString);
 console.log(typeof numberInString); 
+
+
+/*****************STRING OPERATIONS****************** */
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3); //Power
+// console.log(2/3);
+// console.log(2%3);  //Remender
+
+/**********CONCATINATION*********** */
+
+// let str1 = "Hello";
+// let str2 = " Gourav";
+// let str3 = str1+str2;
+// console.log(str3);
+
+// console.log("2" + 3);  //23
+// console.log(2 + "3");  //23
+// console.log("2"+3+3);  //233
+// console.log(2+2+"3");  //43
+
+// console.log((2 + 3) * 7 % 4); 
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 5;      //Code not easy to read for others
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
