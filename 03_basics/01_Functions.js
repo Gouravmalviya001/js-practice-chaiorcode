a5fc70483f319c1733f692770e33324d173f92ab
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-22 11:19:32 +0000

    learned more about functions in js

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -24,7 +24,33 @@ function loginUserMessage(username){
     return `${username}  just logged in.`;
 }
 
-let username1 = loginUserMessage("Gourav123");
-let username2 = loginUserMessage("");
-console.log(username1);
-console.log(username2);
+// let username1 = loginUserMessagen("Gourav123");
+// let username2 = loginUserMessage("");
+// console.log(username1);
+// console.log(username2);
+
+function calculateCartPrice(val1,val2,...num){
+    return val1,val2,num;
+}
+
+const res = calculateCartPrice(100,200,10000);
+console.log(res);
+
+const user = {
+    username : "gourav@123",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`${anyobject.username} is the userame and the price is ${anyobject.price}`);
+}  
+
+handleObject(user);
+
+const myNewArray = [100, 200,400, 600 ];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
