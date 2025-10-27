4f6e713b0f5643651f44406341114dd0dabb7224
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-27 14:16:06 +0000

    learned about for of Loop and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,32 @@
+/*********Loops for Arrays*****/
+
+// const arr = [1, 2, 3, 4, 5, 6];
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greet = "Hello World !";
+
+// for (const str of greet) {
+//     console.log(str);
+// }
+
+//*********MAPS******** */
+
+const map = new Map();
+map.set('IN',"india");
+map.set('USA',"United States of America");
+map.set('Fr',"France");
+map.set('UK',"United Kingdom");
+
+map.set('IN',"india"); //Holding Unique Values only
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key," :- ",value);
+}
+
+// for of are not applicable on objects
+
