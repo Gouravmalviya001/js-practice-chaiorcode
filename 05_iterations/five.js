e30d496e22c18fc8f47c8775d490e56ba1b10c48
Gourav Malviya <gouravmalviya1235@gmail.com>
2025-10-28 11:10:57 +0000

    learn more abour about ForEach Loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -1,5 +1,41 @@
-const coding = ['Js','Cpp','Java','Python','C'];
+// const coding = ['Js','Cpp','Java','Python','C'];
 
-conding.forEach(function (item) {
-    
-})
+// coding.forEach(function (item) {         // 1.
+//     console.log(item);
+// })
+
+// coding.forEach((item) => {              // 2.
+//     console.log(item);
+// })
+
+// function printItem(item){                   // 3.
+//     console.log(item);
+// }
+
+// coding.forEach(printItem);
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item,index,arr);
+// })
+
+// coding.forEach((item) => {
+//     console.log(item)
+// })
+
+const myNum = [1,2,3,4,5,,6,7,8,9,10];
+
+// const newNum = myNum.filter((num) => {
+//     return num < 4;
+// })
+
+// console.log(newNum);
+
+const newNum = [];
+
+myNum.forEach( (num) => {
+    if(num > 4){
+        newNum.push(num);
+    }
+});
+
+console.log(newNum);
